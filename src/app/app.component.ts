8c77cfc3ee9da61b7e2981d5dbc8c0336da0c304
Mostafa <jaouadinna@gmail.com>
2022-09-15 11:57:57 +0100

    basic vedio player component implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'components-deep-dive';
+  name = 'Angular';
+  @ViewChild('videoPlayer', { static: false }) videoplayer!: ElementRef;
+  isPlay: boolean = false;
+
+  playPause() {
+    let myVideo: HTMLVideoElement = this.videoplayer.nativeElement;
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+  }
+
+  skip(value: number) {
+    let myVideo: HTMLVideoElement = this.videoplayer.nativeElement;
+    myVideo.currentTime += value;
+  }
+
+  restart() {
+    let video: HTMLVideoElement = this.videoplayer.nativeElement;
+    video.currentTime = 0;
+  }
+  fullScreen() {
+    let video: HTMLVideoElement = this.videoplayer.nativeElement;
+    video.requestFullscreen();
+  }
 }
